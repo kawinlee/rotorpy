180c4c6635f899cf9f5485b7b6c4aaec0bfa6425
koalasloth <kawinleebkk@gmail.com>
2024-02-06 23:48:57 -0500

    eval auto execution every 5 epochs. Need eval to close after sim ends

--- examples/ppo_hover_eval.py
+++ examples/ppo_hover_eval.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import subprocess
 
 from rotorpy.vehicles.crazyflie_params import quad_params  # Import quad params for the quadrotor environment.
 
@@ -27,191 +28,206 @@ Visualization is slow for this!! To speed things up, we save the figures as indi
 close out of the matplotlib figure things should run faster. You can also speed it up by only visualizing 1 or 2 RL agents. 
 
 """
+def evaluate(auto_mode=False):
+    # First we'll set up some directories for saving the policy and logs.
+    models_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "rotorpy", "learning", "policies", "PPO")
+    log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "rotorpy", "learning", "logs")
+    output_dir = os.path.join(os.path.dirname(__file__), "..", "rotorpy", "data_out", "ppo_hover")
 
-# First we'll set up some directories for saving the policy and logs.
-models_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "rotorpy", "learning", "policies", "PPO")
-log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "rotorpy", "learning", "logs")
-output_dir = os.path.join(os.path.dirname(__file__), "..", "rotorpy", "data_out", "ppo_hover")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    models_available = os.listdir(models_dir)
+    #assigning placeholder value for assignment based on type of execution
+    model_idx = None
+    # We briefly handle the case where ppo_hover_eval is ran automatically
+    if auto_mode:
+        # The automatic case always runs the latest model
+        model_idx = len(models_available) - 1
+    else:
+        # List the models here and let the user select which one. 
+        print("Select one of the models:")
+        for i, name in enumerate(models_available):
+            print(f"{i}: {name}")
+        model_idx = int(input("Enter the model index: "))  
+    
+    num_timesteps_dir = os.path.join(models_dir, models_available[model_idx])
+    # Next import Stable Baselines.
+    try:
+        import stable_baselines3
+    except:
+        raise ImportError('To run this example you must have Stable Baselines installed via pip install stable_baselines3')
 
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    from stable_baselines3 import PPO                                   # We'll use PPO for training.
+    from stable_baselines3.ppo.policies import MlpPolicy                # The policy will be represented by an MLP
 
-# List the models here and let the user select which one. 
-print("Select one of the models:")
-models_available = os.listdir(models_dir)
-for i, name in enumerate(models_available):
-    print(f"{i}: {name}")
-model_idx = int(input("Enter the model index: "))  
-num_timesteps_dir = os.path.join(models_dir, models_available[model_idx])
+    # Choose the weights for our reward function. Here we are creating a lambda function over hover_reward.
+    reward_function = lambda obs, act: hover_reward(obs, act, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e-5})
 
-# Next import Stable Baselines.
-try:
-    import stable_baselines3
-except:
-    raise ImportError('To run this example you must have Stable Baselines installed via pip install stable_baselines3')
+    # Set up the figure for plotting all the agents.
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
 
-from stable_baselines3 import PPO                                   # We'll use PPO for training.
-from stable_baselines3.ppo.policies import MlpPolicy                # The policy will be represented by an MLP
+    # Make the environments for the RL agents.
+    num_quads = 10
+    def make_env():
+        return gym.make("Quadrotor-v0", 
+                    control_mode ='cmd_motor_speeds', 
+                    reward_fn = reward_function,
+                    quad_params = quad_params,
+                    max_time = 5,
+                    world = None,
+                    sim_rate = 100,
+                    render_mode='3D',
+                    render_fps = 60,
+                    fig=fig,
+                    ax=ax,
+                    color='b')
 
-# Choose the weights for our reward function. Here we are creating a lambda function over hover_reward.
-reward_function = lambda obs, act: hover_reward(obs, act, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e-5})
+    envs = [make_env() for _ in range(num_quads)]
 
-# Set up the figure for plotting all the agents.
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
+    # Lastly, add in the baseline (SE3 controller) environment.
+    envs.append(gym.make("Quadrotor-v0", 
+                    control_mode ='cmd_motor_speeds', 
+                    reward_fn = reward_function,
+                    quad_params = quad_params,
+                    max_time = 5,
+                    world = None,
+                    sim_rate = 100,
+                    render_mode='3D',
+                    render_fps = 60,
+                    fig=fig,
+                    ax=ax,
+                    color='k'))  # Geometric controller
 
-# Make the environments for the RL agents.
-num_quads = 10
-def make_env():
-    return gym.make("Quadrotor-v0", 
-                control_mode ='cmd_motor_speeds', 
-                reward_fn = reward_function,
-                quad_params = quad_params,
-                max_time = 5,
-                world = None,
-                sim_rate = 100,
-                render_mode='3D',
-                render_fps = 60,
-                fig=fig,
-                ax=ax,
-                color='b')
+    # Print out policies for the user to select.
+    def extract_number(filename):
+        return int(filename.split('_')[1].split('.')[0])
+    num_timesteps_list = [fname for fname in os.listdir(num_timesteps_dir) if fname.startswith('hover_')]
+    num_timesteps_list_sorted = sorted(num_timesteps_list, key=extract_number)
 
-envs = [make_env() for _ in range(num_quads)]
+    if auto_mode:
+        #selecting the latest epoch if ran in auto_mode
+        num_timesteps_idxs = [len(num_timesteps_list_sorted) - 1]
+    else:
+        # Allowing the user to select if not auto_mode
+        print("Select one of the epochs:")
+        for i, name in enumerate(num_timesteps_list_sorted):
+            print(f"{i}: {name}")
+        num_timesteps_idxs = [int(input("Enter the epoch index: "))]  
 
-# Lastly, add in the baseline (SE3 controller) environment.
-envs.append(gym.make("Quadrotor-v0", 
-                control_mode ='cmd_motor_speeds', 
-                reward_fn = reward_function,
-                quad_params = quad_params,
-                max_time = 5,
-                world = None,
-                sim_rate = 100,
-                render_mode='3D',
-                render_fps = 60,
-                fig=fig,
-                ax=ax,
-                color='k'))  # Geometric controller
+    # You can optionally just hard code a series of epochs you'd like to evaluate all at once.
+    # e.g. num_timesteps_idxs = [0, 1, 2, ...]
 
-# Print out policies for the user to select.
-def extract_number(filename):
-    return int(filename.split('_')[1].split('.')[0])
-num_timesteps_list = [fname for fname in os.listdir(num_timesteps_dir) if fname.startswith('hover_')]
-num_timesteps_list_sorted = sorted(num_timesteps_list, key=extract_number)
+    # Evaluation...
+    for (k, num_timesteps_idx) in enumerate(num_timesteps_idxs):  # For each num_timesteps index...
 
-print("Select one of the epochs:")
-for i, name in enumerate(num_timesteps_list_sorted):
-    print(f"{i}: {name}")
-num_timesteps_idxs = [int(input("Enter the epoch index: "))]  
+        print(f"[ppo_hover_eval.py]: Starting epoch {k+1} out of {len(num_timesteps_idxs)}.")
 
-# You can optionally just hard code a series of epochs you'd like to evaluate all at once.
-# e.g. num_timesteps_idxs = [0, 1, 2, ...]
+        # Load the model for the appropriate epoch.
+        model_path = os.path.join(num_timesteps_dir, num_timesteps_list_sorted[num_timesteps_idx])
+        print(f"Loading model from the path {model_path}")
+        model = PPO.load(model_path, env=envs[0], tensorboard_log=log_dir)
 
-# Evaluation...
-for (k, num_timesteps_idx) in enumerate(num_timesteps_idxs):  # For each num_timesteps index...
+        # Set figure title for 3D plot.
+        fig.suptitle(f"Model: PPO/{models_available[model_idx]}, Num Timesteps: {extract_number(num_timesteps_list_sorted[num_timesteps_idx]):,}")
 
-    print(f"[ppo_hover_eval.py]: Starting epoch {k+1} out of {len(num_timesteps_idxs)}.")
+        # Visualization is slow, so we'll also save frames to make a GIF later.
+        # Set the path for these frames here. 
+        frame_path = os.path.join(output_dir, num_timesteps_list_sorted[num_timesteps_idx][:-4])
+        if not os.path.exists(frame_path):
+            os.makedirs(frame_path)
 
-    # Load the model for the appropriate epoch.
-    model_path = os.path.join(num_timesteps_dir, num_timesteps_list_sorted[num_timesteps_idx])
-    print(f"Loading model from the path {model_path}")
-    model = PPO.load(model_path, env=envs[0], tensorboard_log=log_dir)
+        # Collect observations for each environment.
+        observations = [env.reset()[0] for env in envs]
 
-    # Set figure title for 3D plot.
-    fig.suptitle(f"Model: PPO/{models_available[model_idx]}, Num Timesteps: {extract_number(num_timesteps_list_sorted[num_timesteps_idx]):,}")
+        # This is a list of env termination conditions so that the loop only ends when the final env is terminated. 
+        terminated = [False]*len(observations)
 
-    # Visualization is slow, so we'll also save frames to make a GIF later.
-    # Set the path for these frames here. 
-    frame_path = os.path.join(output_dir, num_timesteps_list_sorted[num_timesteps_idx][:-4])
-    if not os.path.exists(frame_path):
-        os.makedirs(frame_path)
+        # Arrays for plotting position vs time. 
+        T = [0]
+        x = [[obs[0] for obs in observations]]
+        y = [[obs[1] for obs in observations]]
+        z = [[obs[2] for obs in observations]]
 
-    # Collect observations for each environment.
-    observations = [env.reset()[0] for env in envs]
+        j = 0  # Index for frames. Only updated when the last environment runs its update for the time step. 
+        while not all(terminated):
+            frames = []  # Reset frames. 
+            for (i, env) in enumerate(envs):  # For each environment...
+                env.render() 
 
-    # This is a list of env termination conditions so that the loop only ends when the final env is terminated. 
-    terminated = [False]*len(observations)
+                if i == len(envs)-1:  # If it's the last environment, run the SE3 controller for the baseline. 
 
-    # Arrays for plotting position vs time. 
-    T = [0]
-    x = [[obs[0] for obs in observations]]
-    y = [[obs[1] for obs in observations]]
-    z = [[obs[2] for obs in observations]]
+                    # Unpack the observation from the environment
+                    state = {'x': observations[i][0:3], 'v': observations[i][3:6], 'q': observations[i][6:10], 'w': observations[i][10:13]}
 
-    j = 0  # Index for frames. Only updated when the last environment runs its update for the time step. 
-    while not all(terminated):
-        frames = []  # Reset frames. 
-        for (i, env) in enumerate(envs):  # For each environment...
-            env.render() 
+                    # Command the quad to hover.
+                    flat = {'x': [0, 0, 0], 
+                            'x_dot': [0, 0, 0], 
+                            'x_ddot': [0, 0, 0], 
+                            'x_dddot': [0, 0, 0],
+                            'yaw': 0, 
+                            'yaw_dot': 0, 
+                            'yaw_ddot': 0}
+                    control_dict = baseline_controller.update(0, state, flat)
 
-            if i == len(envs)-1:  # If it's the last environment, run the SE3 controller for the baseline. 
+                    # Extract the commanded motor speeds.
+                    cmd_motor_speeds = control_dict['cmd_motor_speeds']
 
-                # Unpack the observation from the environment
-                state = {'x': observations[i][0:3], 'v': observations[i][3:6], 'q': observations[i][6:10], 'w': observations[i][10:13]}
+                    # The environment expects the control inputs to all be within the range [-1,1]
+                    action = np.interp(cmd_motor_speeds, [env.unwrapped.rotor_speed_min, env.unwrapped.rotor_speed_max], [-1,1])
 
-                # Command the quad to hover.
-                flat = {'x': [0, 0, 0], 
-                        'x_dot': [0, 0, 0], 
-                        'x_ddot': [0, 0, 0], 
-                        'x_dddot': [0, 0, 0],
-                        'yaw': 0, 
-                        'yaw_dot': 0, 
-                        'yaw_ddot': 0}
-                control_dict = baseline_controller.update(0, state, flat)
+                    # For the last environment, append the current timestep. 
+                    T.append(env.unwrapped.t)
 
-                # Extract the commanded motor speeds.
-                cmd_motor_speeds = control_dict['cmd_motor_speeds']
+                else: # For all other environments, get the action from the RL control policy. 
+                    action, _ = model.predict(observations[i], deterministic=True)
 
-                # The environment expects the control inputs to all be within the range [-1,1]
-                action = np.interp(cmd_motor_speeds, [env.unwrapped.rotor_speed_min, env.unwrapped.rotor_speed_max], [-1,1])
+                # Step the environment forward. 
+                observations[i], reward, terminated[i], truncated, info = env.step(action)
 
-                # For the last environment, append the current timestep. 
-                T.append(env.unwrapped.t)
+                if i == len(envs)-1:  # Save the current fig after the last agent. 
+                    if env.unwrapped.rendering:
+                        frame = os.path.join(frame_path, 'frame_'+str(j)+'.png')
+                        fig.savefig(frame)
+                        j += 1
 
-            else: # For all other environments, get the action from the RL control policy. 
-                action, _ = model.predict(observations[i], deterministic=True)
+            # Append arrays for plotting.
+            x.append([obs[0] for obs in observations])
+            y.append([obs[1] for obs in observations])
+            z.append([obs[2] for obs in observations])
 
-            # Step the environment forward. 
-            observations[i], reward, terminated[i], truncated, info = env.step(action)
+        # Convert to numpy arrays. 
+        x = np.array(x)
+        y = np.array(y)
+        z = np.array(z)
+        T = np.array(T)
 
-            if i == len(envs)-1:  # Save the current fig after the last agent. 
-                if env.unwrapped.rendering:
-                    frame = os.path.join(frame_path, 'frame_'+str(j)+'.png')
-                    fig.savefig(frame)
-                    j += 1
+        # Plot position vs time. 
+        fig_pos, ax_pos = plt.subplots(nrows=3, ncols=1, num="Position vs Time")
+        fig_pos.suptitle(f"Model: PPO/{models_available[model_idx]}, Num Timesteps: {extract_number(num_timesteps_list_sorted[num_timesteps_idx]):,}")
+        ax_pos[0].plot(T, x[:, 0], 'b-', linewidth=1, label="RL")
+        ax_pos[0].plot(T, x[:, 1:-1], 'b-', linewidth=1)
+        ax_pos[0].plot(T, x[:, -1], 'k-', linewidth=2, label="GC")
+        ax_pos[0].legend()
+        ax_pos[0].set_ylabel("X, m")
+        ax_pos[0].set_ylim([-2.5, 2.5])
+        ax_pos[1].plot(T, y[:, 0], 'b-', linewidth=1, label="RL")
+        ax_pos[1].plot(T, y[:, 1:-1], 'b-', linewidth=1)
+        ax_pos[1].plot(T, y[:, -1], 'k-', linewidth=2, label="GC")
+        ax_pos[1].set_ylabel("Y, m")
+        ax_pos[1].set_ylim([-2.5, 2.5])
+        ax_pos[2].plot(T, z[:, 0], 'b-', linewidth=1, label="RL")
+        ax_pos[2].plot(T, z[:, 1:-1], 'b-', linewidth=1)
+        ax_pos[2].plot(T, z[:, -1], 'k-', linewidth=2, label="GC")
+        ax_pos[2].set_ylabel("Z, m")
+        ax_pos[2].set_ylim([-2.5, 2.5])
+        ax_pos[2].set_xlabel("Time, s")
 
-        # Append arrays for plotting.
-        x.append([obs[0] for obs in observations])
-        y.append([obs[1] for obs in observations])
-        z.append([obs[2] for obs in observations])
+        # Save fig. 
+        fig_pos.savefig(os.path.join(frame_path, 'position_vs_time.png'))
 
-    # Convert to numpy arrays. 
-    x = np.array(x)
-    y = np.array(y)
-    z = np.array(z)
-    T = np.array(T)
+    plt.show()
+    #TODO: need to come up with condition to close the program after all the drones have fallen out of the frame
 
-    # Plot position vs time. 
-    fig_pos, ax_pos = plt.subplots(nrows=3, ncols=1, num="Position vs Time")
-    fig_pos.suptitle(f"Model: PPO/{models_available[model_idx]}, Num Timesteps: {extract_number(num_timesteps_list_sorted[num_timesteps_idx]):,}")
-    ax_pos[0].plot(T, x[:, 0], 'b-', linewidth=1, label="RL")
-    ax_pos[0].plot(T, x[:, 1:-1], 'b-', linewidth=1)
-    ax_pos[0].plot(T, x[:, -1], 'k-', linewidth=2, label="GC")
-    ax_pos[0].legend()
-    ax_pos[0].set_ylabel("X, m")
-    ax_pos[0].set_ylim([-2.5, 2.5])
-    ax_pos[1].plot(T, y[:, 0], 'b-', linewidth=1, label="RL")
-    ax_pos[1].plot(T, y[:, 1:-1], 'b-', linewidth=1)
-    ax_pos[1].plot(T, y[:, -1], 'k-', linewidth=2, label="GC")
-    ax_pos[1].set_ylabel("Y, m")
-    ax_pos[1].set_ylim([-2.5, 2.5])
-    ax_pos[2].plot(T, z[:, 0], 'b-', linewidth=1, label="RL")
-    ax_pos[2].plot(T, z[:, 1:-1], 'b-', linewidth=1)
-    ax_pos[2].plot(T, z[:, -1], 'k-', linewidth=2, label="GC")
-    ax_pos[2].set_ylabel("Z, m")
-    ax_pos[2].set_ylim([-2.5, 2.5])
-    ax_pos[2].set_xlabel("Time, s")
-
-    # Save fig. 
-    fig_pos.savefig(os.path.join(frame_path, 'position_vs_time.png'))
-
-plt.show()
+if __name__ == "__main__":
+    evaluate()
