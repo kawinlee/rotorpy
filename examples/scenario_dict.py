2986282fe2221659a9ee7ae1ec02a82e2dab78e0
koalasloth <kawinleebkk@gmail.com>
2024-04-02 22:51:12 -0400

    updated wind scenarios

--- examples/scenario_dict.py
+++ examples/scenario_dict.py
@@ -4,18 +4,18 @@ from rotorpy.wind.dryden_winds import DrydenGust
 from rotorpy.vehicles.crazyflie_params import quad_params
 
 training_scenarios = {
-    "scenario_1":{
+    "scenario_1: No wind provided":{
                 "id": "Quadrotor-v0", 
                 "control_mode":'cmd_motor_speeds', 
                 "reward_fn" : lambda obs, act: hover_reward(obs, act, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e-5}),
                 "quad_params" : quad_params,
                 "max_time" : 5,
-                "wind_profile" : DrydenGust(dt=1/100, sig_wind=np.array([75,75,30]), altitude=2.0),
+                "wind_profile" : None,
                 "world" : None,
                 "sim_rate" : 100,
                 "render_mode":'None'
     },
-    "scenario_2":{
+    "scenario_2: DrydenGust":{
                 "id": "Quadrotor-v0", 
                 "control_mode":'cmd_motor_speeds', 
                 "reward_fn" : lambda obs, act: hover_reward(obs, act, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e-5}),
